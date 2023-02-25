90bf9c2d1292f75c89b543cc28b5db724180d614
Anthony Ivanov <aivanov@swiftinvention.com>
2023-02-25 11:40:34 +0100

    url changes

--- src/helpers/getEnvironment.ts
+++ src/helpers/getEnvironment.ts
@@ -9,7 +9,7 @@ export const getEnvironment = () => {
   switch (window.location.hostname) {
     case 'auth.mee.foundation': return Environment.prodApp;
     case 'auth-dev.mee.foundation': return Environment.devApp;
-    case 'www.mee.foundation': return Environment.prodWeb;
+    case 'mee.foundation': return Environment.prodWeb;
     case 'www-dev.mee.foundation': return Environment.devWeb;
     default: return Environment.prodWeb;
   }
