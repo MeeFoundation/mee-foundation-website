14552ec2a828074a44d8548df1898e3cac382227
Anthony Ivanov <aivanov@swiftinvention.com>
2023-03-13 14:30:12 +0100

    perspective lines rewrite on css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontSize: {
+        '25xl': '6rem'
+      },
       fontFamily: {
         sans: ["Public Sans", ...defaultTheme.fontFamily.sans],
       },
