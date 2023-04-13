deb0a88d6c3d3e07d0db8918a801a3b8ba16b60e
Pavel Gusev <freezefess@gmail.com>
2023-04-13 20:14:51 +0400

    add pauses on scrolling

---
+++ src/scripts/global.ts
@@ -0,0 +1,18 @@
+export let scrollY = window.scrollY;
+export let isScrollStopped = false;
+
+export const disableScroll = () => {
+  // Get the current page scroll position
+  scrollY = window.scrollY || document.documentElement.scrollTop;
+  isScrollStopped = true;
+
+  // if any scroll is attempted, set this to the previous value
+  window.onscroll = function () {
+    window.scrollTo(0, scrollY);
+  };
+};
+
+export const enableScroll = () => {
+  window.onscroll = function () {};
+  isScrollStopped = false;
+};
