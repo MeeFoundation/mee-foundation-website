b59ab7cfc6e8b3a5de40666a7268066d36f369a0
Anthony Ivanov <aivanov@swiftinvention.com>
2023-05-18 16:50:34 +0200

    image url fix

--- src/legacy/pages/InstallationSucceed.tsx
+++ src/legacy/pages/InstallationSucceed.tsx
@@ -87,7 +87,7 @@ const partnerList: PartnerItem[] = [
     partnerName: 'Mee Foundation',
     partnerUrl: 'https://mee.foundation',
     partnerDisplayedUrl: 'mee.foundation',
-    partnerImageUrl: 'https://mee.foundation/assets/mee_fav_icon.23140c64.svg',
+    partnerImageUrl: 'https://mee.foundation/assets/mee_fav_icon.svg',
     isCertified: true,
   },
   {
