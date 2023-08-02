4520f6003c8440372374b45f26a179200082af28
Sergey Kucherenko <skucherenko@swiftinvention.com>
2023-08-02 15:46:03 +0400

    Add MDX and embedded videos support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mee.foundation',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   build: {
     assets: 'assets'
   },
   experimental: {
     assets: true
-   },  
+  },
   output: 'static',
   vite: {
     build: {
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/[name][extname]',
+          assetFileNames: 'assets/[name][extname]'
         }
       }
     }
-  },  
+  },
   scopedStyleStrategy: 'class'
 });
