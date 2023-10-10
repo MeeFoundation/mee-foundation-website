d215c692e79e3defe76faac3651d542059d7c15e
Anthony Ivanov <aivanov@swiftinvention.com>
2023-10-10 18:29:07 +0200

    headers fix

--- src/legacy/api/ProxyLiveApi.ts
+++ src/legacy/api/ProxyLiveApi.ts
@@ -7,7 +7,8 @@ const client = new BaseRestClient({
   baseURL: svcUrl,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json; charset=utf8',
+    'Content-Type': 'application/json',
+    
   },
 });
 
