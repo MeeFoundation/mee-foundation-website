60dadcf374c6191936f104ac8fffbce9e7db2eb0
Anthony Ivanov <aivanov@swiftinvention.com>
2024-03-22 11:49:49 +0100

    docs links changed

--- src/legacy/components/Footer.tsx
+++ src/legacy/components/Footer.tsx
@@ -4,29 +4,25 @@ import head from '../assets/head.svg';
 // import appStoreImage from '../assets/appStoreButton.svg';
 
 export const Footer: React.FC = () => (
-  <div className="footer mt-16 max-w-full flex flex-col items-shrink relative">
-    <div className="w-full bg-primary flex flex-col items-center z-10">
+  <div className="items-shrink footer relative mt-16 flex max-w-full flex-col">
+    <div className="z-10 flex w-full flex-col items-center bg-primary">
       <div className="w-full px-4 py-5">
-        <div className="flex flex-col gap-4 md:flex-row justify-between items-center text-2xs md:text-sm leading-3 text-primary-content">
+        <div className="text-2xs flex flex-col items-center justify-between gap-4 leading-3 text-primary-content md:flex-row md:text-sm">
           <p className="leading-4">
-            <a href="/privacy-policy">Privacy Policy</a>
-            {' '}
-            |
-            {' '}
-            <a href="https://docs-dev.mee.foundation/">Docs</a>
-            {' '}
-            |
-            {' '}
+            <a href="/privacy-policy">Privacy Policy</a> |{' '}
+            <a href="https://docs.mee.foundation/">Docs</a> |{' '}
             {/* <Link to="/support">Support</Link> */}
           </p>
-          <p className="leading-4">
-            © 2023 The Mee Foundation, Inc.
-          </p>
+          <p className="leading-4">© 2023 The Mee Foundation, Inc.</p>
           <div className="w-50 hidden md:block" />
           {/* <img alt="App Store" src={appStoreImage} /> */}
         </div>
       </div>
     </div>
-    <img src={head.src} alt="mee-illustration-2" className="w-23 h-21 absolute -top-7 right-0 left-0 m-auto" />
+    <img
+      src={head.src}
+      alt="mee-illustration-2"
+      className="absolute -top-7 left-0 right-0 m-auto h-21 w-23"
+    />
   </div>
 );
