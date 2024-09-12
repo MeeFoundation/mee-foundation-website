36ea8dd10315c3e18b53424c3b7aec1349a3b705
Raman Siliuk <rsiluk@swiftinvention.com>
2024-09-12 16:03:39 +0400

    added mee-components, changed og:image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,264 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-const plugin = require('tailwindcss/plugin')
-
-function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
-}
+const plugin = require('tailwindcss/plugin');
+import theme from 'mee-components/tailwind';
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      textColor: {
-        skin: {
-          "blog-base": withOpacity("--color-text-base"),
-          "blog-accent": withOpacity("--color-accent"),
-          "blog-inverted": withOpacity("--color-fill"),
-        },
-      },
-      backgroundColor: {
-        skin: {
-          "blog-fill": withOpacity("--color-fill"),
-          "blog-accent": withOpacity("--color-accent"),
-          "blog-inverted": withOpacity("--color-text-base"),
-          "blog-card": withOpacity("--color-card"),
-          "blog-card-muted": withOpacity("--color-card-muted"),
-        },
-      },
-      outlineColor: {
-        skin: {
-          "blog-fill": withOpacity("--color-accent"),
-        },
-      },
-      borderColor: {
-        skin: {
-          "blog-line": withOpacity("--color-border"),
-          "blog-fill": withOpacity("--color-text-base"),
-          "blog-accent": withOpacity("--color-accent"),
-        },
-      },
-      fill: {
-        skin: {
-          "blog-base": withOpacity("--color-text-base"),
-          "blog-accent": withOpacity("--color-accent"),
-        },
-        "blog-transparent": "transparent",
-      },   
-      fontSize: {
-        '3xl': ['1.75rem', '1.5'],
-        '3.5xl': ['1.875rem', '1.5rem'],
-        '7xl': ['4rem', '1.3'],
-        '25xl': '6rem',
-        'meeBtn': ['22px', '26px'],
-      },
-      transitionDelay: {
-        200: '200ms',
-        400: '400ms',
-        800: '800ms',
-        1000: '1000ms',
-        1200: '1200ms',
-        1400: '1400ms',
-        1600: '1600ms',
-        1800: '1800ms',
-        2000: '2000ms',
-        2200: '2200ms',
-      },
-      lineHeight: {
-        thick: '1.3',
-      },
-      fontFamily: {
-        sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
-        imperial: ['NYT Imperial', ...defaultTheme.fontFamily.sans],
-      },
-      boxShadow: {
-        button: '1px 1px 5px rgba(0, 0, 0, 0.12)',
-      },
-      spacing: {
-        6.5: '1.625rem',
-        7.5: '1.875rem',
-        10.5: '2.625rem',
-        12.5: '3.125rem',
-        13: '3.25rem',
-        13.5: '3.375rem',
-        14.5: '3.625rem',
-        15: '3.75rem',
-        16.5: '4.125rem',
-        17.5: '4.375rem',
-        21: '5.25rem',
-        22.5: '5.625rem',
-        23: '5.75rem',
-        23.5: '5.875rem',
-        24.5: '6.125rem',
-        25: '6.25rem',
-        25: '6.25rem',
-        30: '7.5rem',
-        33: '8.25rem',
-        34.5: '8.625rem',
-        35.5: '8.875rem',
-        44.5: '11.125rem',
-        45: '11.25rem',
-        58: '14.5rem',
-        73: '18.25rem',
-      },
-      width: {
-        4.5: '1.125rem',
-        'about-sm': '20.9375rem',
-        about: '21.25rem',
-        "about-lg": '16.75rem',
-        160: '40rem',
-      },
-      maxWidth: {
-        111: '27.75rem',
-      },
-      gap: {
-        18.5: '4.625rem',
-        50.5: '12.625rem',
-      },
-      zIndex: {
-        70: '70',
-        100: '100',
-        200: '200',
-        300: '300',
-        999: '999',
-      },
-      borderRadius: {
-        '1/2': '50%',
-        '2.5xl': '20px',
-      },
-      transitionDuration: {
-        400: '400ms',
-        800: '800ms',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': {opacity: 0},
-          '100%': {opacity: 1},
-        },
-        fadeOut: {
-          '0%': {opacity: 1},
-          '100%': {opacity: 0},
-        },
-        riseIn: {
-          '0%': {transform: 'translateY(100%)'},
-          '100%': {transform: 'translateY(0)'},
-        },
-        collapse: {
-          '0%': {transform: 'scale(1)'},
-          '100%': {transform: 'scale(0)'},
-        },
-        collapse3d: {
-          '0%': {transform: 'scale3d(1,1,1)'},
-          '100%': {transform: 'scale3d(0,0,0)'},
-        },
-        moveCard: {
-          '0%': {opacity: 0, transform: 'translateX(-50%)'},
-          '100%': {opacity: 1, transform: 'translateX(0)'},
-        },
-        drawUnderline: {
-          '0%': {width: '0%'},
-          '100%': {width: '100%'},
-        },
-        pulseArrow: {
-          '0%': {transform: 'translateX(0)'},
-          '50%': {transform: 'translateX(6px)'},
-          '100%': {transform: 'translateX(0)'},
-        },
-      },
-      animation: {
-        'riseIn-short': 'riseIn 400ms ease-out both',
-        riseIn: 'riseIn 800ms ease-out both',
-        'riseIn-long': 'riseIn 1000ms ease-out both',
-        fadeIn: 'fadeIn 400ms ease-in both',
-        'fadeIn-long': 'fadeIn 800ms ease-in both',
-        fadeOut: 'fadeOut 400ms ease-out both',
-        drawUnderline: 'drawUnderline 400ms linear both',
-        'moveCard-short': 'moveCard 400ms ease-out both',
-        moveCard: 'moveCard 800ms ease-out both',
-        pulseArrow: 'pulseArrow 800ms ease-out',
-
-        'fadeIn-short': 'fadeIn 250ms ease-in-out forwards',
-        'fadeOut-short': 'fadeOut 200ms ease-in-out forwards',
-        'fadeOut-long': 'fadeOut 1.5s ease-in-out forwards',
-        collapse: 'collapse 1.5s ease-in-out forwards',
-        collapse3d: 'collapse3d 1.5s ease-in-out forwards',
-      },
-    },
-    colors: {
-      transparent: 'transparent',
-      black02: '#00000020',
-      'about-bg1': '#FCE1B1',
-      'about-bg2': '#ECE3AC',
-      'about-bg3': '#C7D1B2',
-      'about-bg4': '#A5C1C5',
-      'about-bg5': '#f5e6b0',
-      'how-me-header-bg': '#fde3d1',
-      'primary': '#4F868E',
-      black: {
-        DEFAULT: '#000',
-      },
-      white: {
-        DEFAULT: '#fff',
-        trans70: '#ffffffb3'
-      },
-      gray: {
-        philippine: '#90878F',
-        light: '#767676',
-        DEFAULT: '#4D4D4D',
-        dark: '#444444',
-        900: '#111827'
-      },
-      blue: {
-        cadet: '#5299A3',
-        sky: '#7ED9E7',
-        DEFAULT: '#447F88',
-        dark: '#4F668E',
-      },
-      yellow: {
-        lighter: '#CBBC99',
-        light: '#F9DF89',
-        DEFAULT: '#FFDF80',
-        dark: '#C5C05C',
-      },
-      orange: {
-        lighter: '#FCDCBB',
-        light: '#F9B689',
-        dark: '#8E764F',
-      },
-      green: {
-        moss: '#858E4F',
-        DEFAULT: '#668E4F',
-      },
-      brown: {
-        spicy: '#8E564F',
-      },
-      purple: {
-        light: '#8E4F85',
-        DEFAULT: '#BC52AD',
-        dark: '#764F8E',
-      },
-    },                 
-  },
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/mee-components/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: theme,
   plugins: [
-    require("@tailwindcss/typography"),
-    require("daisyui"),
-    plugin(({ matchUtilities, theme }) => {
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    plugin(({matchUtilities, theme}) => {
       matchUtilities(
         {
-          "animation-delay": (value) => {
+          'animation-delay': (value) => {
             return {
-              "animation-delay": value,
+              'animation-delay': value,
             };
           },
         },
         {
-          values: theme("transitionDelay"),
-        }
+          values: theme('transitionDelay'),
+        },
       );
     }),
   ],
@@ -266,19 +30,19 @@ module.exports = {
     themes: [
       {
         meeTheme: {
-          primary: "#4E868E",
-          "primary-content": "#FFFFFF",
-          secondary: "#F1D77A",
-          "secondary-content": "#F5F6F6",
-          accent: "#F5B68926",
-          neutral: "#333C4D",
+          primary: '#4E868E',
+          'primary-content': '#FFFFFF',
+          secondary: '#F1D77A',
+          'secondary-content': '#F5F6F6',
+          accent: '#F5B68926',
+          neutral: '#333C4D',
 
-          "base-100": "#FFFFFF", // background
+          'base-100': '#FFFFFF', // background
 
-          info: "#3ABFF8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#E66C4E",
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#E66C4E',
         },
       },
     ],
